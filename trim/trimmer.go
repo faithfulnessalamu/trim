009aae2df6d33b77699d179383e17c688eab391c
Faithfulness Alamu <vaguemail369@gmail.com>
2020-05-16 22:26:42 +0100

    Use backtick for regex string

--- trim/trimmer.go
+++ trim/trimmer.go
@@ -11,6 +11,6 @@ func GetTrimmed(longURL string) (string, error) {
 /*isValidURL checks if a url is valid or not.
 Returns true if it is, else false*/
 func isValidURL(url string) bool {
-	matcher := regexp.MustCompile("[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)")
+	matcher := regexp.MustCompile(`[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)`)
 	return matcher.MatchString(url)
 }
