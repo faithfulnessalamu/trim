294699957de631e04a7b55808cd814911a8f3992
Faithfulness Alamu <vaguemail369@gmail.com>
2020-05-16 20:03:19 +0100

    Spin up a basic server
    Create a simple handler for the root path which returns a hello string

--- trim.go
+++ trim.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
+)
 
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Welcome to Trim")
+}
+
+func main() {
+	http.HandleFunc("/", homeHandler)
+	http.ListenAndServe(":8080", nil)
 }
