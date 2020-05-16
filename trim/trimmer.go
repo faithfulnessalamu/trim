22a14cbf88c0e96f78a1b919b8f3a65def66344c
Faithfulness Alamu <vaguemail369@gmail.com>
2020-05-16 20:19:06 +0100

    Update GetTrimmed function signature

--- trim/trimmer.go
+++ trim/trimmer.go
@@ -1,5 +1,13 @@
 package trim
 
-func GetTrimmed(longlink string) (string, error) {
+/*GetTrimmed generates and returns a shorter link from longURL
+It returns an error if trimming fails for some reason*/
+func GetTrimmed(longURL string) (string, error) {
+
+}
+
+/*isValidURL checks if a url is valid or not.
+Returns true if it is, else false*/
+func isValidURL(url string) bool {
 
 }
