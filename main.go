96ed215978bb3fa91b59e29e0ebe1b713fdff530
Faithfulness Alamu <vaguemail369@gmail.com>
2020-05-17 07:53:44 +0100

    Handle trim paths
    Return a trimmed url to client

--- main.go
+++ main.go
@@ -3,14 +3,27 @@ package main
 import (
 	"fmt"
 	"net/http"
-)
 
+	"github.com/vague369/trim/trim"
+)
 
 func main() {
 	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/trim", trimHandler)
 	http.ListenAndServe(":8080", nil)
 }
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Welcome to Trim")
+}
+
+func trimHandler(w http.ResponseWriter, r *http.Request) {
+	query := r.URL.Query()
+	longURL := query.Get("longUrl")
+
+	trimmedURL, err := trim.GetTrimmed(longURL)
+	if err != nil {
+		fmt.Fprintln(w, err)
+	}
+	fmt.Fprintln(w, trimmedURL)
 }
