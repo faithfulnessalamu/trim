2a4801f46caabe7a92d04fd0bcb318f83dd15115
Faithfulness Alamu <vaguemail369@gmail.com>
2020-05-18 08:05:01 +0100

    Initialize a map storage
    Implement SavePair which saves a trimmedURL, longURL pair

--- data/database.go
+++ data/database.go
@@ -1,6 +1,9 @@
 package data
 
+var storage = make(map[string]string)
+
 /*SavePair persists the url pairs for retrieval later*/
 func SavePair(trimmedURL, longURL string) error {
-
+	storage[trimmedURL] = longURL
+	return nil
 }
