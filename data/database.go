ca02cd12b81875fc6177ffc7375ed5d49e0aa946
Faithfulness Alamu <vaguemail369@gmail.com>
2020-05-18 08:07:22 +0100

    Return an error if key exists in database

--- data/database.go
+++ data/database.go
@@ -1,9 +1,16 @@
 package data
 
+import "errors"
+
+var errorKeyExists = errors.New("Key Exists")
 var storage = make(map[string]string)
 
 /*SavePair persists the url pairs for retrieval later*/
 func SavePair(trimmedURL, longURL string) error {
+	_, exists := storage[trimmedURL]
+	if exists {
+		return errorKeyExists
+	}
 	storage[trimmedURL] = longURL
 	return nil
 }
