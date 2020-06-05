499783252f8ee103fe123653b017cc0bb3f9d916
Faithfulness Alamu <vaguemail369@gmail.com>
2020-06-05 16:59:00 +0100

    Create custom errors for any backing database

--- memdatabase.go
+++ memdatabase.go
@@ -1,9 +1,5 @@
 package main
 
-import (
-	"errors"
-)
-
 //memDatabase implements the database interface.
 //It stores data in memory and is useful for testing
 //For simplicity, stores strings only
@@ -14,7 +10,7 @@ type memDatabase struct {
 func (m *memDatabase) save(key string, value string) error {
 	// Check key existence
 	if _, ok := m.data[key]; ok {
-		return errors.New("Key exists already")
+		return errKeyExists
 	}
 	m.data[key] = value
 	return nil
@@ -23,7 +19,7 @@ func (m *memDatabase) save(key string, value string) error {
 func (m *memDatabase) retrieve(key string) (string, error) {
 	value, ok := m.data[key]
 	if !ok {
-		return "", errors.New("Key does not exist")
+		return "", errKeyNotFound
 	}
 	return value, nil
 }
