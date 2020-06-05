0c85dbac1e89d6f535af33275c27f015459e116c
Faithfulness Alamu <vaguemail369@gmail.com>
2020-06-05 11:42:22 +0100

    Serve static files

--- trim.go
+++ trim.go
@@ -11,6 +11,9 @@ func main() {
 	r.HandleFunc("/{hash}", RedirectHandlerFunc).Methods(http.MethodGet)
 	// Serve the home page
 	r.Handle("/", http.FileServer(http.Dir(".")))
+	// Serve static files
+	statics := r.PathPrefix("/static/")
+	statics.Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
 
 	http.ListenAndServe(":8080", r)
 }
