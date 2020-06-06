58246364886f7dc6d099b0a289a7de1dca6133e5
Faithfulness Alamu <vaguemail369@gmail.com>
2020-06-06 10:50:50 +0100

    Add url validator

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/thealamu/trim
 
 go 1.14
 
-require github.com/gorilla/mux v1.7.4
+require (
+	github.com/gorilla/mux v1.7.4
+	github.com/stretchr/testify v1.6.1
+)
---
+++ validation_test.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestIsValidURL(t *testing.T) {
+	goodURL1 := isValidURL("https://www.github.com/github")
+	goodURL2 := isValidURL("http://homeofthefree.org")
+
+	badURL1 := isValidURL("hpt//isnota.protocol")
+	badURL2 := isValidURL("http://...")
+
+	require := require.New(t)
+
+	require.True(goodURL1, "https://www.github.com/github should be a valid URL")
+	require.True(goodURL2, "http://homeofthefree.org should be a valid URL")
+
+	require.False(badURL1, "hpt//isnota.protocol should not be a valid URL")
+	require.False(badURL2, "http://... should not be a valid URL")
+}
