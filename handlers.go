e4ca2c1c8e5818a06e6f614e2fdc3cc623a53a17
Faithfulness Alamu <vaguemail369@gmail.com>
2020-06-07 20:44:01 +0100

    Perform redirects

--- handlers.go
+++ handlers.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"net/http"
 	"path/filepath"
 
@@ -83,6 +82,6 @@ func RedirectHandler(db database) func(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 		infoLogger.Printf("Redirecting to %s", actualURL)
-		fmt.Fprintf(w, "Redirecting now to %s", actualURL)
+		http.Redirect(w, r, actualURL, http.StatusTemporaryRedirect)
 	}
 }
